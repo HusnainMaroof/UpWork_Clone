474f63ac8b2f4b6421cda030ed768e9e7cc778f7
HusnainDot <husnainmaroof95@gmail.com>
2025-08-02 12:12:27 +0500

    seting up Frontend Registration part

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./globals.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
+const App = () => {
+  return (
+    <>
+      <Toaster />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
+    </>
+  );
+};
+
+export default App;
